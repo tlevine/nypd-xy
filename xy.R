a43d402f1f20f32cb074229b10038f564bca4cb0
Thomas Levine <_@thomaslevine.com>
2014-12-23 08:24:49 +0000

    assume a flat earth

---
+++ xy.R
@@ -0,0 +1,30 @@
+if (!'crime' %in% ls())
+  crime <- read.csv('data/02378420399528461352-11853667273131550346.csv')
+cr <- crime[sample.int(nrow(crime), 1e3),]
+
+unscientific <- function(x) format(x, scientific = F)
+
+write.formula <- function(model) {
+  b0 <- coef(model)[1]
+  b1 <- coef(model)[2]
+  y <- names(attr(lat$terms, 'dataClasses'))[[1]]
+  x1 <- names(attr(lat$terms, 'dataClasses'))[[1]]
+
+  paste(format(y, scientific = FALSE),
+        '=',
+        format(b0, scientific = FALSE),
+        '+',
+        format(b1, scientific = FALSE),
+        '×',
+        format(x1, scientific = FALSE))
+}
+
+main <- function() {
+  long <- lm(longitude ~ X, data = crime)
+  lat <- lm(latitude ~ Y, data = crime)
+  cat(paste('Assuming a flat Earth,',
+            write.formula(long),
+            write.formula(lat),
+            '',
+            sep = '\n'))
+}
